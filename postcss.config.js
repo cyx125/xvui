57b154237f6431dd7d30caaf7c2b48dbcc832d42
chenyuxiao <836924178@qq.com>
2018-08-27 11:15:11 +0800

    postcss配置(区分mp端和weex端)

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,15 @@
-module.exports = {
-  plugins: [
-    require('postcss-plugin-weex')(),
-    require('autoprefixer')({
-      browsers: ['> 0.1%', 'ios >= 8', 'not ie < 12']
-    }),
+const plugins = [
+  require('autoprefixer')({
+    browsers: ['> 0.1%', 'ios >= 8', 'not ie < 12']
+  })
+];
+if (process.env.RUN_TYPE !== 'WECHATMP') {
+  plugins.push(require('postcss-plugin-weex')());
+  plugins.push(
     require('postcss-plugin-px2rem')({
       rootValue: 75,
       minPixelValue: 1.01
     })
-  ]
-};
+  )
+}
+module.exports = { plugins: plugins }
